4615ae5c243ed2ee75a50f574419bbbc78a9d66c
BananaBoii <98301106+BananaBoii600@users.noreply.github.com>
2022-04-07 21:35:44 +0530

    Overall GUI Changed

--- main.py
+++ main.py
@@ -1,66 +1,55 @@
-#packages
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+import sys
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 
 
-#Creating the main browser class
-class WebBrowser(QMainWindow):
-
+class MainWindow(QMainWindow):
     def __init__(self):
-        self.window = QWidget()
-        self.window.setWindowTitle("Web Browser")
-
-        self.layout = QVBoxLayout()
-        self.horizontal = QHBoxLayout()
-
-        #Creating the buttons
-        self.url_bar = QTextEdit()
-        self.url_bar.setMaximumHeight(30)
-
-        self.go_btn = QPushButton("Go")
-        self.go_btn.setMinimumHeight(30)
-
-        self.back_btn = QPushButton("<")
-        self.back_btn.setMinimumHeight(30)
-
-        self.forward_btn = QPushButton(">")
-        self.forward_btn.setMinimumHeight(30)
-
-        #Adding the Buttons to the canvas
-        self.horizontal.addWidget(self.back_btn)
-        self.horizontal.addWidget(self.forward_btn)
-        self.horizontal.addWidget(self.url_bar)
-        self.horizontal.addWidget(self.go_btn)
-
+        super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
 
-        #Creating Functions for the buttons
-        self.go_btn.clicked.connect(lambda: self.navigate(self.url_bar.toPlainText()))
-        self.back_btn.clicked.connect(self.browser.back)
-        self.forward_btn.clicked.connect(self.browser.forward)
-                
-        self.layout.addLayout(self.horizontal)
-        self.layout.addWidget(self.browser)
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
 
-        #Setting the browser's default url when opened to google.com
-        self.browser.setUrl(QUrl("https://google.com"))
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
 
-        self.window.setLayout(self.layout)
-        self.window.show()
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
 
-    #Function to add "http:// to the url if it is not typed already"
-    def navigate(self, url):
-        if not url.startswith("http"):
-            url = "http://" + url
-            self.url_bar.setText(url)
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
         self.browser.setUrl(QUrl(url))
 
-    def home(self):
-        self.url_bar.setText("google.com")
-        self.navigate()
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
 
-app = QApplication([])
-window = WebBrowser()
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Web Browser')
+window = MainWindow()
 app.exec_()
